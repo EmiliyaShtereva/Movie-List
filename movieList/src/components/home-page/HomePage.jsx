259794f6aa10220997d47e558e60b610d47e1b59
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-27 15:05:45 +0200

    added home page file

---
+++ movieList/src/components/home-page/HomePage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../footer/Footer";
+import NavBar from "../navbar/NavBar";
+import Table from "../table/Table";
+
+export default function HomePage() {
+    return (
+        <>
+            <NavBar />
+            <Table />
+            <Footer />
+        </>
+    )
+}
