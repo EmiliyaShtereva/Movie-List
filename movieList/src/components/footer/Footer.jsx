6a7748d938f5da62cec2f00ac4c6d6a64018fcaf
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-27 20:15:30 +0200

    removed social media buttons

--- movieList/src/components/footer/Footer.jsx
+++ movieList/src/components/footer/Footer.jsx
@@ -4,19 +4,13 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <div className={styles['social-media']}>
-                    <button><i className="fa-brands fa-facebook"></i></button>
-                    <button><i className="fa-brands fa-instagram"></i></button>
-                    <button><i className="fa-brands fa-x-twitter"></i></button>
-                    <button><i className="fa-brands fa-discord"></i></button>
-                </div>
                 <div className={styles["other-pages"]}>
                     <a href="">Home</a>
-                    <a href="">About Us</a>
+                    <a href="">About</a>
                     <a href="">Contacts</a>
                 </div>
                 <div className={styles["copyright"]}>
-                    <p>Copyright &copy;2024 Site name</p>
+                    <p>This site is created for training purposes</p>
                 </div>
             </footer>
         </>
