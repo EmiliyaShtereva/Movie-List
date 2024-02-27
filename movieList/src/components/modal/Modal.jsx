33422719dc1fffc355b4e59c49263961675daea0
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-27 20:16:01 +0200

    added a modal for adding friends

---
+++ movieList/src/components/modal/Modal.jsx
@@ -0,0 +1,23 @@
+import styles from './Modal.module.css'
+
+export default function Modal() {
+    return (
+        <div className={styles["modal"]}>
+            <div className={styles["modal-content"]}>
+                <span className={styles["close"]}>&times;</span>
+                <form className={styles["search-form"]}>
+                    <input type="text" placeholder="Search friend..." name="search" />
+                    <button type="submit"><i className="fa fa-search"></i></button>
+                </form>
+                <p>Invited Friends:</p>
+                <div className={styles["friends"]}>
+                    <p>First Last</p>
+                    <p>First Last</p>
+                    <p>First Last</p>
+                    <p>First Last</p>
+                    <p>First Last</p>
+                </div>
+            </div>
+        </div>
+    )
+}
