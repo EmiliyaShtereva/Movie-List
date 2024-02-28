18f6b299068628239c9d3c8b213896ab49d38a39
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-28 20:02:51 +0200

    removed other pages from footer

--- movieList/src/components/footer/Footer.jsx
+++ movieList/src/components/footer/Footer.jsx
@@ -1,14 +1,10 @@
+import { Link } from 'react-router-dom'
 import styles from './Footer.module.css'
 
 export default function Footer() {
     return (
         <>
             <footer>
-                <div className={styles["other-pages"]}>
-                    <a href="">Home</a>
-                    <a href="">About</a>
-                    <a href="">Contacts</a>
-                </div>
                 <div className={styles["copyright"]}>
                     <p>This site is created for training purposes</p>
                 </div>
