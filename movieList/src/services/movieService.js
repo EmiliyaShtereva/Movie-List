49f81ab7176b37ddef5e2244a6811db2dbeb9006
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-29 22:45:04 +0200

    added movie service

---
+++ movieList/src/services/movieService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:3000/movies';
+
+export const getOnePage = async (currentPage) => {
+    const response = await axios.get(`${baseUrl}?_page=${currentPage}&_per_page=5`);
+    const result = await response.data;
+    return result;
+}
