ff9080d141d063d12043178c39a6a6f90e02b15f
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-02-29 22:39:48 +0200

    removed routing

--- movieList/src/App.jsx
+++ movieList/src/App.jsx
@@ -1,13 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./components/home-page/HomePage"
 
 function App() {
   return (
-    <BrowserRouter> 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <HomePage />
   )
 }
 
