a46d8dafe6e41ca3a51aac0623559ee4bca169ac
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-03-01 23:41:40 +0200

    added functionality for movie search autocomplete and modal for the movies

--- movieList/src/components/navbar/NavBar.jsx
+++ movieList/src/components/navbar/NavBar.jsx
@@ -1,15 +1,45 @@
+import { useEffect, useState } from 'react';
+import * as movieService from '../../services/movieService'
+import useSearch from '../../hooks/useSearch'
 import styles from './NavBar.module.css'
+import MovieSearchModal from '../modal/MovieSearchModal';
 
 export default function NavBar() {
+    let [movies, setMovies] = useState([]);
+    let [showMovieModal, setShowMovieModal] = useState(false);
+    let [suggestionIndex, setSuggestionIndex] = useState(0);
+    let { value, suggestions, onChange } = useSearch(movies);
+
+    const movieClickHandler = (index) => {
+        setSuggestionIndex(index);
+        setShowMovieModal(true);
+    }
+
+    useEffect(() => {
+        movieService.getMovies()
+            .then(result => {
+                setMovies(result);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
     return (
+        <>
         <header>
             <nav>
                 <div>
-                    <form className={styles["search-form"]}>
-                        <input type="text" placeholder="Search movie..." name="search" />
+                    <form className={styles["search-form"]} autoComplete='off'>
+                        <input type="text" placeholder="Search movie..." name="search" onChange={onChange} value={value} />
+                        <ul className={styles["search-suggestions"]}>
+                            {suggestions.map((s, i) => (
+                                <li key={s.id} className={styles["suggestion"]} onClick={() => movieClickHandler(i)}>{s.name}</li>
+                            ))}
+                        </ul>
                     </form>
                 </div>
             </nav>
         </header>
+        {showMovieModal && <MovieSearchModal onClose={() => setShowMovieModal(false)} {...suggestions[suggestionIndex]}/>}
+        </>
     )
 }
