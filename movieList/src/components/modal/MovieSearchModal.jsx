33316cd1ff34800aa83cf80a2d9525023525c5b5
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-03-03 22:27:28 +0200

    added search and update for friends to the movie search modal

--- movieList/src/components/modal/MovieSearchModal.jsx
+++ movieList/src/components/modal/MovieSearchModal.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 import * as friendsService from '../../services/friendsService'
+import * as movieService from '../../services/movieService'
 import styles from './MovieSearchModal.module.css'
 import useSearch from '../../hooks/useSearch';
 
 export default function MovieSearchModal({ onClose, duration, invitedFriends, id, img, name, rating, synopsis }) {
     let [friends, setFriends] = useState([]);
+    let [friendsToInvite, setFriendsToInvite] = useState([...invitedFriends]);
+    let [showSuggestions, setShowSuggestions] = useState(false);
+    let [successfulyInvited, setSuccessfulyInvited] = useState(false);
     let { value, suggestions, onChange } = useSearch(friends);
 
     useEffect(() => {
@@ -13,35 +17,71 @@ export default function MovieSearchModal({ onClose, duration, invitedFriends, id
                 setFriends(result);
             })
             .catch(err => console.log(err));
-    }, [])
+    }, []);
+
+    const friendClickHandler = (friendId, friendName) => {
+        for (let i = 0; i < friendsToInvite.length; i++) {
+            if (friendsToInvite[i].name === friendName) {
+                setShowSuggestions(false);
+                return friendsToInvite;
+            }
+        }
+        setShowSuggestions(false);
+        setFriendsToInvite(state => ([...state, { id: friendId, name: friendName }]));
+    }
+
+    const inviteFriendsHandler = async (e) => {
+        e.preventDefault();
+        await movieService.addFriendsToList(img, name, duration, rating, synopsis, id, friendsToInvite)
+            .then(() => {
+                setSuccessfulyInvited(true);
+            })
+            .catch(err => console.log(err));
+    }
 
     return (
         <div className={styles["modal"]}>
-            <div className={styles["backdrop"]} onClick={onClose}></div>
+            <div className={styles["backdrop"]} onClick={() => {onClose(); setSuccessfulyInvited(false);}}></div>
             <div className={styles["modal-content"]}>
-                <span className={styles["close"]} onClick={onClose}>&times;</span>
+                <span className={styles["close"]} onClick={() => {onClose(); setSuccessfulyInvited(false);}}>&times;</span>
                 <div className={styles['movie-image']}>
                     <img src={img} alt="movie name" />
                 </div>
                 <div className={styles['movie-info']}>
                     <h1>{name}</h1>
-                    <p><p className={styles['headers']}>Duration:</p> {duration}</p>
-                    <p><p className={styles['headers']}>Rating:</p> {rating}</p>
-                    <p><p className={styles['headers']}>Synopsis:</p> {synopsis}</p>
+                    <p className={styles['headers']}>Duration:</p> <p>{duration}</p>
+                    <p className={styles['headers']}>Rating:</p> <p>{rating}</p>
+                    <p className={styles['headers']}>Synopsis:</p> <p>{synopsis}</p>
                 </div>
                 <form className={styles["search-form"]} autoComplete='off'>
-                    <input type="text" placeholder="Search friend..." name="search" onChange={onChange} value={value} />
-                    <button type="submit">Invite friends</button>
-                    <ul className={styles["search-suggestions"]}>
-                        {suggestions.map((s) => (
-                            <li key={s.id} className={styles["suggestion"]}>{s.name}</li>
-                        ))}
-                    </ul>
+                    <input
+                        type="text"
+                        placeholder="Search friend..."
+                        name="search"
+                        onClick={() => setShowSuggestions(true)}
+                        onChange={onChange}
+                        value={value}
+                    />
+                    <button type="submit" onClick={inviteFriendsHandler}>Invite friends</button>
+                    {successfulyInvited && <p className={styles["successfuly-invited"]}>Successfuly Invited Friends</p>}
+                    {showSuggestions && (
+                        <ul className={styles["search-suggestions"]}>
+                            {suggestions.map((s) => (
+                                <li
+                                    key={s.id}
+                                    className={styles["suggestion"]}
+                                    onClick={() => friendClickHandler(s.id, s.name)}
+                                >
+                                    {s.name}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </form>
                 <p>Invited Friends:</p>
                 <div className={styles["friends"]}>
-                    {invitedFriends.map(f => (
-                        <p>{f}</p>
+                    {friendsToInvite.map(f => (
+                        <p key={f.id}>{f.name}</p>
                     ))}
                 </div>
             </div>
