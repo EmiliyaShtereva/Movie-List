0794ab29e4941abfb09acee2e6761a8d158fe766
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-03-04 17:32:34 +0200

    added functionality to fetch friend groups to friends service

--- movieList/src/services/friendsService.js
+++ movieList/src/services/friendsService.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3000/friends';
+const baseUrl = 'http://localhost:3000';
 
 export const getFriends = async () => {
-    const response = await axios.get(baseUrl);
+    const response = await axios.get(`${baseUrl}/friends`);
+    const result = await response.data;
+    return result;
+}
+
+export const getFriendGroups = async () => {
+    const response = await axios.get(`${baseUrl}/friendGroups`);
     const result = await response.data;
     return result;
 }
